bc1f901fc41ac118198fa10d569dce55fd6a0adb
MatthewBotha <m.botha41@gmail.com>
2016-03-17 23:54:07 +0200

    Added GroupTest and removed App and AppTest

---
+++ People/src/test/java/bravopeople/GroupTest.java
@@ -0,0 +1,13 @@
+package bravopeople;
+
+import junit.framework.TestCase;
+
+public class GroupTest extends TestCase
+{
+    public GroupTest(String name) { super(name); }
+
+    public void testGroup()
+    {
+
+    }
+}
