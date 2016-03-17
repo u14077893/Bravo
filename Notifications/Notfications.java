e4fbead1c5c8cfdb7cd505e30e5ebaa1d3f65bbe
Frederick Ehlers <u11061112@tuks.co.za>
2016-03-17 22:46:05 +0200

    just the base file for now so people can do work for tomorrow

---
+++ Notifications/Notfications.java
@@ -0,0 +1,75 @@
+class Notifications{
+	/*
+		@param emailList[] it will be an array containing all the email addresses the report must be sent to
+		@param file this will be the generated report file that needs to be sent as an attachment with the email
+	*/
+	public void sendReport(String emailList[], String file){
+		/*
+			This function must take the list of emails and add each one to be sent to, ask buildMessage to generate the appropriate message
+			and receive the email's body from buildMessage.
+
+			We are in the process of making a telnet server to interface to the up mail server so just code as far as you can then be able to explain
+			the work you have done thus far.
+		*/
+	}
+
+	/*
+		@param emailList[] it will be an array containing all the email addresses the notification must be sent to
+		other parameters still unsure
+	*/
+	public void sendNotification(String emailList[]){
+		/*
+			The email list must receive their notifications as per request, a sql request must be made to pull the notification message as the user speicifed
+			when the notification was created.
+
+			Thus each mail must be sent one by one so each user can get his/her personalised notfication.
+
+			This will not use buildMessage as the user specified his own message.
+		*/
+	}
+
+	/*
+		@param emailList[] it will be an array containing all the email addresses the change notice must be sent to
+		other parameters still unsure
+	*/
+	public void sendChange(String emailList[]){
+		/*
+			All authors must be notified if someone made changes to their publication of any kind. (Note the publication name must be specified as an author can belong to multiple groups) 
+			Still to be figured out if it will tell the authors exactly what was changed. (to be discussed)
+		*/
+	}
+
+	/*
+		@param emailList[] it will be an array containing all the email addresses the change notice must be sent to
+		other parameters still unsure
+	*/
+	public void sendTextMail(String emailList[]){
+		/*
+			This will be used to email plain text emails no images/files etc. 
+			will use buildMessage to assist the contruction of the message.
+		*/
+	}
+
+	/*
+		@param typeOfRequest specifies the type of request it will be eg a notification or a change notice.
+	*/
+	private String buildMessage(String typeOfRequest){ 
+		/*
+			This will generate a kind of "To whom it may concern" message when there are multiple clients in one email.
+			The type of request will let the function know which template file to use.
+		*/
+		return "";
+	}
+
+	/*
+		@param clientName this variable specifies the email receiptiant's name to make the mail look more professional
+		@param typeOfRequest specifies the type of request it will be eg a notification or a change notice.
+	*/
+	private String buildMessage(String clientName, String typeOfRequest){
+		/*
+			This will take the client's name and add it to the message to give it a personal touch
+			The type of request will let the function know which template file to use.
+		*/
+		return "";
+	}
+}
