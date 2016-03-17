c0aa99c9d91036835138a3c8a7e30102f0b2123c
mpho-baloyi <u14133670@tuks.co.za>
2016-03-17 20:33:55 +0200

    Added the Organisation and Person class

---
+++ People/src/Organisations.java
@@ -0,0 +1,33 @@
+/*
+	Author: Mpho Baloyi 
+	Student Number: 14133670
+	Group Name: Bravo-People
+	
+	This file defines the Organization Class for the Researcher Support System (RSS).
+*/
+
+//package people;
+
+class Organisation
+{
+	String name;
+	//String ID;
+	
+	Organisation()
+	{
+		name="Organisation";
+	}
+	
+	Organisation(String organisation_name)
+	{
+		name = organisation_name;
+	}
+	
+	public void setName(String name)
+	{}
+		
+	public void String getName()
+	{
+		return name;
+	}
+}
