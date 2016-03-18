db0245fd05134fdcf1f190593facdbc1ce7d8667
Azhar Mohungoo <azhar.m.ish@gmail.com>
2016-03-18 15:20:19 +0200

    Client.java Skeleton

---
+++ Notifications/Client.java
@@ -0,0 +1,37 @@
+import java.net.*;
+import java.io.*;
+
+public class Client extends Thread
+{
+    public Client(String host, Integer port)
+    {
+        
+    }
+
+    public void terminate()
+    {
+        
+    }
+
+    @Override
+    public void start()
+    {
+        
+    }
+
+    @Override
+    public void run()
+    {
+        
+    }
+
+    public void send( String msg )
+    {
+       
+    }
+
+    public static void main(String args[])
+    {
+
+    }
+}
