8917df54d407442d75770878dd3171d1b789d8a8
Hugo-Meiring <wh.meiring@gmail.com>
2016-03-18 02:27:07 +0200

    Updated Group file and created empty Entity class to inherit from

---
+++ People/src/main/java/bravopeople/Entity.java
@@ -0,0 +1,9 @@
+package people;
+
+/**
+ * <h1>Entity</h1>
+ * Basic empty Entity class used by group
+ * @author Bravo Team
+ * @version 1.0
+ */
+public class Entity {}
--- People/src/main/java/bravopeople/Group.java
+++ People/src/main/java/bravopeople/Group.java
@@ -2,15 +2,26 @@ package bravopeople;
 
 class Group {
 	private String name;
-	// Requires more fields
+	private ArrayList<Entity> entities;		// Composite Pattern
 
+	public Group() {
+		this.name = "MockGroup";
+		this.entities = new ArrayList<Entity>();
+	}
 	// Basic constructor
-	public Group(String name) {
+	public Group(String name, ArrayList<Entity> entities) {
 		this.name = name;
+		this.entities = entities;
 	}
 
-	public String getName()
-	{
-		return name;
+	public String getName() { return name; }
+	public ArrayList<Entity> getEntities() { return entities; }
+
+	public void setName(String name) {
+		this.name = name;
+	}	
+
+	public void setEntities(ArrayList<Entity> entities) {
+		this.entities = entities;
 	}
 }
