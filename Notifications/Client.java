ec8f5c9fd1bc2ca67d90bdf353520dbd58e34c44
Azhar Mohungoo <azhar.m.ish@gmail.com>
2016-03-18 15:23:36 +0200

    Client Constructor and Main

--- Notifications/Client.java
+++ Notifications/Client.java
@@ -3,9 +3,25 @@ import java.io.*;
 
 public class Client extends Thread
 {
+    Socket sock;
+
+    volatile PrintWriter writer;
+    volatile BufferedReader reader;
+    volatile boolean running = false;
+
     public Client(String host, Integer port)
     {
-        
+        super();
+        try{
+          Socket sock = new Socket(host, port);
+
+          writer = new PrintWriter( sock.getOutputStream() );
+
+          reader = new BufferedReader(
+                         new InputStreamReader( sock.getInputStream() ) );
+        } catch ( IOException e ){
+            e.toString();
+        }
     }
 
     public void terminate()
@@ -32,6 +48,24 @@ public class Client extends Thread
 
     public static void main(String args[])
     {
+        Client c = new Client("kendy.up.ac.za", 25);
+        c.start();
 
+        try
+        {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String msg;
+
+            while(c.running)
+            {
+              c.send(msg);
+            } 
+
+            c.terminate();
+        }
+        catch(Exception e)
+        {
+            e.toString();
+        }
     }
 }
