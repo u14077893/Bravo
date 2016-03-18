29e77b3b3a6fd5bb7014cda63d7271a23398c9e1
Mosesjoseph <mayimela@gmail.com>
2016-03-18 10:02:40 +0200

    Added getPublication test

---
+++ Maven1/src/main/java/Publications.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author moses
+ */
+public interface Publications 
+{
+    public  Publication getPublication(String t);
+}
