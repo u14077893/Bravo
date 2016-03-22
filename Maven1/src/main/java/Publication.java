5dbc1e64e6b680853766ae1d45582c0d46e4fd64
u13133064 <u13133064@tuks.co,za>
2016-03-22 22:20:00 +0200

    Revert "Removed Mave1 from Master branch"
    This reverts commit 1a759526686ea884247e4e6afa64286a72157305.

---
+++ Maven1/src/main/java/Publication.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author moses
+ */
+public interface Publication 
+{
+    public String getType();
+
+    public void setType(String type);
+
+    public String getName();
+
+    public void setName(String name);
+
+    public String getURL();
+
+    public void setURL(String URL);
+    
+    
+
+    public String getTitle();
+    public void setTitle(String title);
+}
