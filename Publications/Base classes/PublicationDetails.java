bdf03df6e0b10a30b68a291ae7dbeb9dc640c43f
Hlengi <u14077893@tuks.co.za>
2016-03-23 22:35:15 +0200

    Publication Details
    Added java class file to base classes

---
+++ Publications/Base classes/PublicationDetails.java
@@ -0,0 +1,35 @@
+
+package za.ac.cs.teambravo.publications.publication_details;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Hlengekile
+ */
+public class PublicationDetails
+{
+    private String pubTitle;
+    private Date envisagedPublicationDate;
+    
+    public void setTitle(String inTitle)
+    {
+        pubTitle = inTitle;
+    }
+    
+    public void setEnvisagedPublicationDate(Date inDate)
+    {
+        envisagedPublicationDate = inDate;
+    }
+    
+    public String getTitle()
+    {
+        return pubTitle;
+    }
+    
+    public Date getEnvisagedPublicationDate()
+    {
+        return envisagedPublicationDate;
+    }
+    
+}
