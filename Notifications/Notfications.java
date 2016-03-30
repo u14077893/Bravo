1b073a673452cc76a7e2ea067fd7069d04af7df7
Frederick Ehlers <u11061112@tuks.co.za>
2016-03-30 21:31:03 +0200

    Changes to Build Message, code was in a netbeans package ensure you have the right package

--- Notifications/Notfications.java
+++ Notifications/Notfications.java
@@ -1,3 +1,5 @@
+package com.bravonotifications.notifications;
+
 class Notifications{
 	/*
 		@param emailList[] it will be an array containing all the email addresses the report must be sent to
@@ -10,7 +12,11 @@ class Notifications{
 
 			We are in the process of making a telnet server to interface to the up mail server so just code as far as you can then be able to explain
 			the work you have done thus far.
+            
+                        Submitting an email to be sent must be done one email address at a time so all looping must be done here.
 		*/
+            String message = "LOL";
+            submitMultipartMail(emailList[0], message,file);
 	}
 
 	/*
@@ -19,58 +25,67 @@ class Notifications{
 	*/
 	public void sendNotification(String emailList[]){
 		/*
-			The email list must receive their notifications as per request, a sql request must be made to pull the notification message as the user speicifed
-			when the notification was created.
+			The email list must receive their notifications as per request, 
 
 			Thus each mail must be sent one by one so each user can get his/her personalised notfication.
 
-			This will not use buildMessage as the user specified his own message.
+			This will still buildMessage as that function will get the personalised message from the data base.
 		*/
 		String message = "Sample message"; //this will be replaced with code to fetch the message stored on the database
 		message = message + "Sent via automatic notifications";
 		
-		submitEmail(emailList[i], "Automated Notification", message);
+		submitTextMail(emailList[0], message);
 	}
-
+        
+        /*
+            @param emailList[] the list of people that need to receive their reminders
+        */
+        public void sendReminder(String emailList[]){
+            
+        }
+        
 	/*
 		@param emailList[] it will be an array containing all the email addresses the change notice must be sent to
-		other parameters still unsure
+		@param message this will be the generated message from our side to be sent to the user via a simple text only mail
 	*/
-	public void sendChange(String emailList[], String publicationName){
-		/*
-			All authors must be notified if someone made changes to their publication of any kind. (Note the publication name must be specified as an author can belong to multiple groups) 
-			Still to be figured out if it will tell the authors exactly what was changed. (to be discussed)
-		*/
-		String message = "";
-		
-		for (int i = 0; i < emailList.length; i ++){
-			if (emailList[i] != null && !emailList[i].isEmpty()){
-				message = buildMessage(emailList[i], "Change", publicationName)
-				//submitEmail(email_Address, email_subject, email_message)
-				submitEmail(emailList[i], "Change in Publication", message);
-			}
-		}
-	}
-
-	/*
-		@param emailList[] it will be an array containing all the email addresses the change notice must be sent to
-		other parameters still unsure
-	*/
-	public void sendTextMail(String emailList[]){
+	public void submitTextMail(String email, String message){
 		/*
 			This will be used to email plain text emails no images/files etc. 
 			will use buildMessage to assist the contruction of the message.
 		*/
-		String recipientList=emailList[0];
-		String message="sample message"; //the message to be sent to the recipient
+		String recipientList= email;
+		message = "sample message"; //the message to be sent to the recipient
 		String subje="subject of message";
 		//list of recipients seperated by commas : e.g banele@gmail.com , u12201911@tuks.co.za , mm@webmaster.com
-			for (int i=1;i<emailList.length;i++)
+			/*for (int i=1;i<email.length;i++)
 			{
 				recipientList+=","+emailList[i];
-			}
+			}*/
 			//address,subject,message
-			submitEmail(recipientList,subje,message);
+			//submitEmail(recipientList,subje,message);
+			
+	}
+        
+        /*
+		@param emailList[] it will be an array containing all the email addresses the change notice must be sent to
+		@param message this will be the generated message from our side to be sent to the user via a simple text only mail
+                @param file (type still to be discussed) the file that has to be attached to the email.
+	*/
+        public void submitMultipartMail(String email, String message, String file){
+		/*
+			This will be used to email plain text emails no images/files etc. 
+			will use buildMessage to assist the contruction of the message.
+		*/
+		String recipientList = email;
+		message= "sample message"; //the message to be sent to the recipient
+		String subje= "subject of message";
+		//list of recipients seperated by commas : e.g banele@gmail.com , u12201911@tuks.co.za , mm@webmaster.com
+			/*for (int i=1;i<emailList.length;i++)
+			{
+				recipientList+=","+emailList[i];
+			}*/
+			//address,subject,message
+			//submitEmail(recipientList,subje,message);
 			
 	}
 
@@ -79,55 +94,54 @@ class Notifications{
 	*/
 
 	/*
-		@param clientName this variable specifies the email receiptiant's name to make the mail look more professional may be null;
+		@param email this variable allows the sql request to the data base to allow us to get teh user's name for the mail;
 		@param typeOfRequest specifies the type of request it will be eg a notification or a change notice.
-		@param publicationName is the name of the publication that is included in the report or has been changed. This field can be null
 	*/
-	private String buildMessage(String clientName, String typeOfRequest, String publicationName){ // Frederick
+	private String buildMessage(String email, String typeOfRequest){ // Frederick
 		/*
 			This will take the client's name and add it to the message to give it a personal touch
 			The type of request will let the function know which template file to use.
 		*/
-		String message = "";
-		
-		if((typeOfRequest.equals("Change") && (!publicaionName.equals(""))){
-			message = publicationMessage(publicationName);
+                String username = "";// sql request using email to identify the user 
+		String message  = ""; 
+		if(typeOfRequest.equals("Reminder")){
+                        String pm = ""; //sql request to get personal message set by user
+			message = reminderMessage(username,pm);                        
 		}
-		else if((typeOfRequest.equals("Notification")) && (!clientName.equals("")){
-			message = notificationMessage(clientName);
+		else if(typeOfRequest.equals("Notification")){
+			message = notificationMessage(username);
 		}
-		else if((typeOfRequest.equals("Report")) && (!publicaionName.equals("")){
-			message = reportMessage(publicationName);
+		else if(typeOfRequest.equals("Report")){
+			message = reportMessage(username);
 		}
-		else {
-			message = "";
-		}
+
 		return message;
 	}
 	
 	/*Functions to help mock build message's functionality */
 
-	private String publicationMessage(String publicationName){
-		String intro = "To whom it may concern \n";
+	private String reminderMessage(String user, String pmessage){
+                String publicationName = "Random Title"; //sql request to database
+		String intro = "Good day " + user + "\n";
 		String body = "Please note that the following publication has been modified:";
-		String salutation = "Regards \n Bravo team"
+		String salutation = "Regards \n Bravo team";
 		String completed = intro + body + publicationName + "\n" + salutation;
 		return completed;
 	}
 
-	private String notificationMessage(String clientName){
-		String intro = "Dear " + clientName + "\n";				// sql request to sql database to get notification message
+	private String notificationMessage(String user){
+		String intro = "Good day " + user + "\n";				// sql request to sql database to get notification message
 		String body = "This is the user's personal message";
-		String salutation = "Regards \n Bravo team"
+		String salutation = "Regards \n Bravo team";
 		String completed = intro + body + "\n" + salutation;
 		return completed;
 	}
 
-	private String reportMessage(Srting publicationName){
-		String intro = "To whom it may concern \n";				// sql request to sql database to get the report file
+	private String reportMessage(String user){
+		String intro = "Good day " + user + "\n";				// sql request to sql database to get the report file
 		String body = "Plesae find attached the report file for: ";
-		String salutation = "Regards \n Bravo team"
-		String completed = intro + body + publicationName + "\n" + salutation;
+		String salutation = "Regards \n Bravo team";
+		String completed = intro + body + "\n" + salutation;
 		return completed;
 
 		//message = clientName + ", \n" + typeOfRequest;
@@ -137,3 +151,23 @@ class Notifications{
 	
 
 }
+
+/*
+Not needed functions:
+
+public void sendChange(String emailList[], String publicationName){
+		/*
+			All authors must be notified if someone made changes to their publication of any kind. (Note the publication name must be specified as an author can belong to multiple groups) 
+			Still to be figured out if it will tell the authors exactly what was changed. (to be discussed)
+                        Submitting an email to be sent must be done one email address at a time so all looping must be done here.
+		*//*
+		String message = "";
+		
+		for (int i = 0; i < emailList.length; i ++){
+			if (emailList[i] != null && !emailList[i].isEmpty()){
+				message = buildMessage(emailList[i], "Change", publicationName);
+				submitTextMail(emailList[i], message);
+			}
+		}
+	}
+*/
