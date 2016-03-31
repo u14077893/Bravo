c6cd343ef0672c22a1c93f169080684e036847fd
Dillon Heins <dillonheins@gmail.com>
2016-03-31 18:02:40 +0200

    Reporting module added - WIP

---
+++ Integration/RSS/reporting_ejb/src/main/java/bravo/reporting/ejb/ReportingBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bravo.reporting.ejb;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Dillon
+ */
+@Stateless
+@LocalBean
+public class ReportingBean {
+    public void businessMethod() {
+    }
+}
