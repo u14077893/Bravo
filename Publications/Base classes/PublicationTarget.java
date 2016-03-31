55baf5bb9938f3035de35a4ec322b5ff355502b1
Mosesjoseph <mayimela@gmail.com>
2016-03-31 02:40:13 +0200

    Refactored PubilcationTarget to PublicationTarget

---
+++ Publications/Base classes/PublicationTarget.java
@@ -0,0 +1,33 @@
+
+import java.net.URL;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kudzai
+ */
+public class PublicationTarget {
+    String name;
+    URL website;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public URL getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(URL website) {
+        this.website = website;
+    }
+}
