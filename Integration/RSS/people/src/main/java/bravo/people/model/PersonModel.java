494ed5409c1de14c0712aae4d03312ee6d006e56
Dillon Heins <dillonheins@gmail.com>
2016-04-07 00:25:24 +0200

    Integration - WIP

---
+++ Integration/RSS/people/src/main/java/bravo/people/model/PersonModel.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bravo.people.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Dillon
+ */
+@XmlRootElement
+public class PersonModel {
+    private String firstName;
+    private String surname;
+    private String staffNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getStaffNumber() {
+        return staffNumber;
+    }
+
+    public void setStaffNumber(String staffNumber) {
+        this.staffNumber = staffNumber;
+    }
+    
+}
--- Integration/RSS/people/src/main/java/bravo/people/resource/PeopleResource.java
+++ Integration/RSS/people/src/main/java/bravo/people/resource/PeopleResource.java
@@ -7,8 +7,12 @@ package bravo.people.resource;
 
 import javax.ws.rs.Path;
 import bravo.people.ejb.*;
+import bravo.people.model.*;
 import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -18,19 +22,35 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("people")
 public class PeopleResource {
-    @EJB
-    private PeopleBean peopleBean;
+    /*@POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("addPerson")
+    public void addPerson(@FormParam("firstName") String firstName, @FormParam("surname") String surname, @FormParam("staffNumber") String staffNumber) {
+        peopleBean.addPerson(firstName, surname, staffNumber);
+    }*/
     
-    @GET
-    @Path("createPerson")
-    public void createPerson() {
-        peopleBean.createPerson("Dillon", "Heins", "14035538");
+    @POST
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    @Path("addPerson")
+    public void addPerson(PersonModel personModel) {
+        System.out.println(personModel.getFirstName());
+//        peopleBean.addPerson(personModel.getFirstName(), personModel.getSurname(), personModel.getStaffNumber());
     }
     
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    @Path("getFirstName")
-    public String getFirstName() {
-        return peopleBean.getFirstName();
+    @Path("getPerson")
+    public String getPerson() {
+        String persons = "";
+        
+        for (int i = 0; i < peopleBean.getPerson().size(); ++i) {
+            persons += peopleBean.getPerson().get(i) + "\n";
+        }
+        
+        return persons;
     }
+    
+    @EJB
+    private PeopleBean peopleBean;
 }
