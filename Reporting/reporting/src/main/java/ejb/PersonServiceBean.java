605aef3c879f1449d18c48d0f8307aa16b40f865
Einsteinium1458 <meyers.charl@gmail.com>
2016-04-08 19:11:13 +0200

    Added simple ejb with unit test

--- Reporting/reporting/src/main/java/ejb/PersonServiceBean.java
+++ Reporting/reporting/src/main/java/ejb/PersonServiceBean.java
@@ -1,18 +1,16 @@
 package ejb;
 
+import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.*;
 import entity.*;
 
-@Stateless
-public class PersonBean {
+@LocalBean
+@Stateless(mappedName="PersonServiceBean")
+public class PersonServiceBean {
 	//Pass persistence unit to entity manager
 	@PersistenceContext(unitName = "reporting")
 	private EntityManager entityManager;
-	
-	public void addPerson(Person p) {
-		entityManager.persist(p);
-	}
 	
 	public Person getPersonByID(int id) {
 		return entityManager.find(Person.class, id);
