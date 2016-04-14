65860762721d763d9063f4476be769366705c6e0
vuyaniShabangu <vuyani.shabangu@gmail.com>
2016-04-14 18:58:31 +0200

    Added fields and their setters and getters

--- Publications/Bravo_Publications/Bravo_Publications-ejb/src/main/java/za/ac/cs/teambravo/publications/requestandresponses/ModifyPublicationTypeRequest.java
+++ Publications/Bravo_Publications/Bravo_Publications-ejb/src/main/java/za/ac/cs/teambravo/publications/requestandresponses/ModifyPublicationTypeRequest.java
@@ -1,5 +1,7 @@
 package za.ac.cs.teambravo.publications.requestandresponses;
 import za.ac.cs.teambravo.publications.base.*;
+import java.util.Date;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,30 +13,11 @@ import za.ac.cs.teambravo.publications.base.*;
  * @author User
  */
 public class ModifyPublicationTypeRequest implements PublicationRequest{
-    private String name; //the name of the publication type that you want to modify
-    private PublicationTypeState publicationTypeStateObject; //the publication type state with new effective date
+    private PublicationType publicationTypeObject;
+    private PublicationTypeState publicationTypeStateObject;
     
-    //constructor:
-    public ModifyPublicationTypeRequest(String name, PublicationTypeState publicationTypeStateObject) {
-        this.name = name;
-        this.publicationTypeStateObject = publicationTypeStateObject;
+    public ModifyPublicationTypeRequest(String publicationTypeName, Date effectiveDate){
+        publicationTypeObject.setPublicationType(publicationTypeName);
+        publicationTypeStateObject.setEffectiveDate(effectiveDate);
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public PublicationTypeState getPublicationTypeStateObject() {
-        return publicationTypeStateObject;
-    }
-
-    public void setPublicationTypeStateObject(PublicationTypeState publicationTypeStateObject) {
-        this.publicationTypeStateObject = publicationTypeStateObject;
-    }
-    
-    
 }
