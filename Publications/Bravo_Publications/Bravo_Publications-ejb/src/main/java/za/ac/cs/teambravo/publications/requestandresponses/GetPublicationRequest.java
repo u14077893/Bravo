a341e915c5c4831a532726ec0b7947d1ceb28663
Mosesjoseph <mayimela@gmail.com>
2016-04-15 06:22:36 +0200

    Implmimented GetPublication request and response

--- Publications/Bravo_Publications/Bravo_Publications-ejb/src/main/java/za/ac/cs/teambravo/publications/requestandresponses/GetPublicationRequest.java
+++ Publications/Bravo_Publications/Bravo_Publications-ejb/src/main/java/za/ac/cs/teambravo/publications/requestandresponses/GetPublicationRequest.java
@@ -10,6 +10,20 @@ package za.ac.cs.teambravo.publications.requestandresponses;
  *
  * @author User
  */
-public class GetPublicationRequest implements PublicationRequest{
+public class GetPublicationRequest implements PublicationRequest
+{
+    public String title;
+
+    public GetPublicationRequest(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
     
 }
--- Publications/Bravo_Publications/Bravo_Publications-ejb/src/main/java/za/ac/cs/teambravo/publications/requestandresponses/GetPublicationResponse.java
+++ Publications/Bravo_Publications/Bravo_Publications-ejb/src/main/java/za/ac/cs/teambravo/publications/requestandresponses/GetPublicationResponse.java
@@ -1,5 +1,8 @@
 package za.ac.cs.teambravo.publications.requestandresponses;
 
+import javax.persistence.Entity;
+import za.ac.cs.teambravo.publications.entities.PublicationEntity;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +13,19 @@ package za.ac.cs.teambravo.publications.requestandresponses;
  *
  * @author User
  */
-public class GetPublicationResponse implements PublicationResponse{
-    
+public class GetPublicationResponse implements PublicationResponse
+{
+    public PublicationEntity publicationObject;
+
+    public GetPublicationResponse(PublicationEntity publicationObject) {
+        this.publicationObject = publicationObject;
+    }
+
+    public PublicationEntity getPublicationObject() {
+        return publicationObject;
+    }
+
+    public void setPublicationObject(PublicationEntity publicationObject) {
+        this.publicationObject = publicationObject;
+    }
 }
