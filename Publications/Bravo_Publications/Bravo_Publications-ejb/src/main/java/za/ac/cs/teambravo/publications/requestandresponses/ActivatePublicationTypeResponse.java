8f5f1be5ecbfb8f03dd2762a52fcb7ac058c06b3
vuyaniShabangu <vuyani.shabangu@gmail.com>
2016-04-15 09:46:26 +0200

    Implemented the class with typeName field, constructor, getters and setters

--- Publications/Bravo_Publications/Bravo_Publications-ejb/src/main/java/za/ac/cs/teambravo/publications/requestandresponses/ActivatePublicationTypeResponse.java
+++ Publications/Bravo_Publications/Bravo_Publications-ejb/src/main/java/za/ac/cs/teambravo/publications/requestandresponses/ActivatePublicationTypeResponse.java
@@ -10,5 +10,19 @@ package za.ac.cs.teambravo.publications.requestandresponses;
  * @author vuyani
  */
 public class ActivatePublicationTypeResponse {
+    String typeName;
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public ActivatePublicationTypeResponse(String typeName) {
+        this.typeName = typeName;
+    }
+    
     
 }
