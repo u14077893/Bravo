6a20c44888237d85b5a47fca1b0443d287f43858
Mosesjoseph <mayimela@gmail.com>
2016-04-15 05:59:11 +0200

    changed generation type to TABLE in PublicationEntity

--- Publications/Bravo_Publications/Bravo_Publications-ejb/src/main/java/za/ac/cs/teambravo/publications/entities/PublicationEntity.java
+++ Publications/Bravo_Publications/Bravo_Publications-ejb/src/main/java/za/ac/cs/teambravo/publications/entities/PublicationEntity.java
@@ -21,7 +21,7 @@ import javax.persistence.OneToMany;
 public class PublicationEntity implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Integer publicationID;
     
     
