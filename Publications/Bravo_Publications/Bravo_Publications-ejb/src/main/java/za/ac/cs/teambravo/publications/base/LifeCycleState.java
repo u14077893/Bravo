410665e9c5e80e015c6bcb13258fd788dd25c697
Gift-Sefako <giftsefako@gmail.com>
2016-04-15 10:34:54 +0200

    Changing life cycle state to class

--- Publications/Bravo_Publications/Bravo_Publications-ejb/src/main/java/za/ac/cs/teambravo/publications/base/LifeCycleState.java
+++ Publications/Bravo_Publications/Bravo_Publications-ejb/src/main/java/za/ac/cs/teambravo/publications/base/LifeCycleState.java
@@ -1,13 +1,55 @@
 package za.ac.cs.teambravo.publications.base;
 
+import java.util.Date;
+
 
 
 //package za.ac.cs.teambravo.publications.publications;
 
-public interface LifeCycleState {
+public class LifeCycleState {
     
-    public LifeCycleState getState();
-    public void setState(LifeCycleState newState);
-    public String getStateString();
+ //   public LifeCycleState getState();
+  //  public void setState(LifeCycleState newState);
+   // public String getStateString();
+    
+    
+    
+   private String state;
+   private Integer percentageCompleted;
+   private Date publicationDate;
+   private String bibTexEntry;
+
+    public String getState() {
+        return state;
+    }
+     public String getBibTexEntry() {
+        return bibTexEntry;
+    }
+
+    public Integer getPercentageCompleted() {
+        return percentageCompleted;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setPercentageCompleted(Integer percentageCompleted) {
+        this.percentageCompleted = percentageCompleted;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public void setBibTexEntry(String bibTexEntry) {
+        this.bibTexEntry = bibTexEntry;
+    }
+    
+    
     
 }
